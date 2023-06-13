36ea910e1db8f9f33edd0ee46d7c93850b3348eb
Victor Matheus <victormatheus183@gmail.com>
2023-06-13 18:52:10 -0300

    Timer for level countdown created
    Uses the TimeFormatter and fire events when the countdown reaches zero

---
+++ Assets/Scripts/Managers/Timer.cs
@@ -0,0 +1,69 @@
+﻿namespace Assets.Scripts.Managers
+{
+    using TMPro;
+    using UnityEngine;
+    using Assets.Scripts.Services;
+    using UnityEngine.Events;
+
+    public class Timer : MonoBehaviour
+    {
+        public UnityEvent OnTimeEnds;
+
+        [SerializeField] private int _maxTimeInSeconds = 120;
+        [SerializeField] private TextMeshProUGUI _timerTMP;
+
+        private float _currentTime;
+        private bool _shouldCount;
+
+ 
+        void Start()
+        {
+            ResetTimer();
+        }
+
+        public void ResetTimer()
+        {
+            _currentTime = _maxTimeInSeconds;
+            UpdateTimerTMP(_currentTime);
+        }
+
+        public void StartTimer()
+        {
+            _shouldCount = true;
+        }
+
+        private void Update()
+        {
+            CountDown();
+            FireEventsWhenTimeReachesZeroThenStopCounting();
+        }
+
+        private void CountDown()
+        {
+            if (ShouldCount())
+            {
+                _currentTime -= Time.deltaTime;
+                UpdateTimerTMP(_currentTime);
+            }
+        }
+
+        private bool ShouldCount()
+        {
+            return (_shouldCount && _currentTime > 0);
+        }
+
+        private void UpdateTimerTMP(float value)
+        {
+            _timerTMP.text = TimeFormatter.GetTimeFormated(value);
+        }
+
+        private void FireEventsWhenTimeReachesZeroThenStopCounting()
+        {
+            if(_currentTime <= 0)
+            {
+                OnTimeEnds?.Invoke();
+                _shouldCount = false;
+            }
+        }
+    }
+}
