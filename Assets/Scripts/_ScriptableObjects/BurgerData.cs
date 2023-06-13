5d1034bcf5da9cafe0b4a25d0103b5991bd5061d
Victor Matheus <victormatheus183@gmail.com>
2023-06-13 16:27:13 -0300

    Burger Scriptable object created

---
+++ Assets/Scripts/_ScriptableObjects/BurgerData.cs
@@ -0,0 +1,15 @@
+﻿namespace Assets.Scripts._ScriptableObjects
+{
+    #if UNITY_EDITOR
+    using UnityEditor;
+    #endif
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "Burger", menuName = "ScriptableObjects/Burguers")]
+    public class BurgerData : ScriptableObject
+    {
+        public Sprite Icon;
+        public string Name;
+        public GameObject[] Ingredients = new GameObject[3];
+    }
+}
