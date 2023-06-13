4ecd553cd60e0c265a4e9d48a86b8df59e6a8e8d
Victor Matheus <victormatheus183@gmail.com>
2023-06-13 16:53:08 -0300

    minor changes

---
+++ Assets/Scripts/Components/Rotation.cs
@@ -0,0 +1,26 @@
+﻿namespace Assets.Scripts.Components
+{
+    using System.Collections;
+    using UnityEngine;
+    
+    public class Rotation : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void LateUpdate()
+        {
+            transform.Rotate(Vector3.up * Time.deltaTime * 200, Space.Self);
+        }
+    }
+}
