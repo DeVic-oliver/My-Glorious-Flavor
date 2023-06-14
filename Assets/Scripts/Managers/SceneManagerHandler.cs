cf0448a4ebcb99e339cc8287acf9d6af8834c9ee
Victor Matheus <victormatheus183@gmail.com>
2023-06-14 19:08:40 -0300

    Basic scene handler to reload scene
    Reloads scene after a delay caused by a coroutine.
    Prevents multiple coroutines to be started.

---
+++ Assets/Scripts/Managers/SceneManagerHandler.cs
@@ -0,0 +1,29 @@
+﻿namespace Assets.Scripts.Managers
+{
+    using System.Collections;
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class SceneManagerHandler : MonoBehaviour
+    {
+        private Coroutine _currentSceneCoroutine;
+        
+        public void RestartScene()
+        {
+            if(_currentSceneCoroutine != null )
+            {
+                _currentSceneCoroutine = StartCoroutine(ReloadSceneCoroutine());
+            }
+        }
+
+        private IEnumerator ReloadSceneCoroutine()
+        {
+            float secondsToWait = 1f;
+            yield return new WaitForSeconds(secondsToWait);
+         
+            int sceneID = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneID);
+        }
+
+    }
+}
